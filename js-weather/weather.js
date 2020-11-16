27701ee96cd23fafea9e09b7872ef14e98319066
織田麻子 <12odadao14@gmail.com>
2020-11-16 11:46:47 +0900

    update テンプレートリテラルでの文字列結合に変更

--- js-weather/weather.js
+++ js-weather/weather.js
@@ -33,16 +33,13 @@ document.getElementById('cityId').addEventListener('change', () => {
     //すでに表示されている、weatherInfoオブジェクトのテキストノードの削除
     const text = document.getElementsByClassName('weatherInfo');
     for (let i = 0, len = text.length; i < len; i++) {
-        text[i].removeChild(text[i].firstChild);
+        text[i].textContent = '';
     }
 
     //サービスへの問い合わせURLを生成
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?id=';
-    const cityId = encodeURIComponent(document.getElementById('cityId').value);
-    const callback = "&callback=getWeather&lang=ja&units=metric&appid=4b5774e9f3d2a07b84f0f2f88e486224";
     // <script>タグを生成
     const scr = document.createElement('script');
-    scr.src = apiUrl + cityId + callback;
+    scr.src = `https://api.openweathermap.org/data/2.5/weather?id=${encodeURIComponent(document.getElementById('cityId').value)}&callback=getWeather&lang=ja&units=metric&appid=4b5774e9f3d2a07b84f0f2f88e486224`;
     const lastChild = document.body.lastChild;
     document.body.replaceChild(scr, lastChild);
 }, false);
